7f2c2152d4e922c7bed62599df28a5b38a34ba74
juanfran2000 <72174575+juanfran2000@users.noreply.github.com>
2023-08-01 08:35:39 -0500

    segundo-commit

---
+++ src/app/paginas/page.tsx
@@ -0,0 +1,10 @@
+import MyPetCarousel from "../components/MyPetCarousel";
+
+export default function Ejemplo() {
+  return (
+    <div>
+      <h1 className="text-2xl"> Esta es nuestra pagina de Ejemplo</h1>
+      <MyPetCarousel />
+    </div>
+  );
+}
